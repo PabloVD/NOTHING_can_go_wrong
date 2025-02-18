71386893e0087bdc87c8665e55d192fefc5e6b65
PabloVD <pablo.villanueva.domingo@gmail.com>
2025-02-19 00:17:45 +0100

    Add menu UI, markdown addon

---
+++ scripts/menu.gd
@@ -0,0 +1,41 @@
+extends Control
+
+@onready var mainmenu = $Menus/MainMenu
+@onready var credits = $Menus/Credits
+@onready var controls = $Menus/Controls
+
+func _ready() -> void:
+	credits.visible = false
+	controls.visible = false
+	mainmenu.visible = true
+	#$Menus/Fullscreen.button_pressed = true
+	
+	if DisplayServer.window_get_mode()==DisplayServer.WINDOW_MODE_FULLSCREEN:
+		$Menus/Fullscreen.button_pressed = true
+	elif DisplayServer.window_get_mode()==DisplayServer.WINDOW_MODE_WINDOWED:
+		$Menus/Fullscreen.button_pressed = false
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/Main.tscn")
+
+func _on_controls_pressed() -> void:
+	controls.visible = true
+	mainmenu.visible = false
+
+func _on_credits_pressed() -> void:
+	credits.visible = true
+	mainmenu.visible = false
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
+func _on_back_pressed() -> void:
+	credits.visible = false
+	controls.visible = false
+	mainmenu.visible = true
+
+func _on_fullscreen_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED) 
