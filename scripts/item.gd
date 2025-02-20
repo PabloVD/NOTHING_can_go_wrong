06c6f1bff760e512111fc08e9d06acc4131261d1
daniDTB <databawork@gmail.com>
2025-02-20 20:39:57 +0100

    start coding spawner and minimap

--- scripts/item.gd
+++ scripts/item.gd
@@ -1,9 +1,10 @@
 extends Node3D
 
+var parent
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	parent = get_parent() 
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,5 +14,6 @@ func _process(delta: float) -> void:
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body.is_in_group("robot"):
 		if not body.has_item:
+			parent.item_grab()
 			body.get_item()
 			queue_free()
