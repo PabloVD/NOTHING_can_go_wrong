37ec4a82a6e5ff5f1c2156ed415d3e98ae9be476
PabloVD <pablo.villanueva.domingo@gmail.com>
2025-02-21 16:35:29 +0100

    Start tutorial screen in initial message

--- scripts/initial_message.gd
+++ scripts/initial_message.gd
@@ -1,15 +1,10 @@
 extends Control
+@onready var greetings: Control = $Greetings
+@onready var tutorial: Control = $Tutorial
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+func _on_proceed_pressed() -> void:
+	greetings.visible = false
+	tutorial.visible = true
 
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/World.tscn")
